eec2cbb598bb806882a034f022612da4e103b25c
Amit Leshed <amitleshed@icloud.com>
2025-07-21 11:55:25 -0400

    remove redundant test

--- test/test_algoruby.rb
+++ test/test_algoruby.rb
@@ -6,8 +6,4 @@ class TestAlgoruby < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Algoruby::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
